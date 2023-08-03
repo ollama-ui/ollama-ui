ad79c4b81bf4e08cab50b905c690e19b336db32d
rtcfirefly <rtcfirefly@users.noreply.github.com>
2023-08-03 09:23:08 -0700

    add dropdown to select language model

--- chat.js
+++ chat.js
@@ -1,8 +1,32 @@
+// Fetch available models and populate the dropdown
+async function populateModels() {
+  try {
+    const response = await fetch("http://localhost:11434/api/tags");
+    const data = await response.json();
+    const selectElement = document.getElementById('model-select');
+
+    data.models.forEach(model => {
+      const option = document.createElement('option');
+      option.value = model.name;
+      option.innerText = model.name;
+      selectElement.appendChild(option);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Function to get the selected model
+function getSelectedModel() {
+  return document.getElementById('model-select').value;
+}
+
 // Function to handle the user input and call the API functions
 async function submitRequest() {
   const input = document.getElementById('user-input').value;
-  const data = { model: "llama2", prompt: input };
-  
+  const selectedModel = getSelectedModel();
+  const data = { model: selectedModel, prompt: input };
+
   // Create user message element and append to chat history
   let chatHistory = document.getElementById('chat-history');
   let userMessageDiv = document.createElement('div');
@@ -39,3 +63,6 @@ document.getElementById('user-input').addEventListener('keydown', function (e) {
     submitRequest();
   }
 });
+
+
+window.onload = populateModels;
