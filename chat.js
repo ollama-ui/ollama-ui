b8d939e6738f6ec92b45ff2b68a4b848f333bbfa
rtcfirefly <rtcfirefly@users.noreply.github.com>
2023-09-24 10:10:22 -0700

    network error notification

--- chat.js
+++ chat.js
@@ -1,3 +1,17 @@
+const faqString = `
+**How can I expose the Ollama server?**
+
+By default, Ollama allows cross origin requests from 127.0.0.1 and 0.0.0.0. To support more origins, you can use the OLLAMA_ORIGINS environment variable:
+
+\`\`\`
+OLLAMA_ORIGINS=${window.location.origin} ollama serve
+\`\`\`
+
+Also see: https://raw.githubusercontent.com/jmorganca/ollama/main/docs/faq.md
+`;
+
+
+
 const clipboardIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
 <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
@@ -64,7 +78,13 @@ async function populateModels() {
     }
   }
   catch (error) {
-    console.error(error);
+    document.getElementById('errorText').innerHTML =
+    DOMPurify.sanitize(marked.parse(
+    `Ollama-ui was unable to communitcate with Ollama due to the following error:\n\n`
+    + `\`\`\`${error.message}\`\`\`\n\n---------------------\n`
+    + faqString));
+    let modal = new bootstrap.Modal(document.getElementById('errorModal'));
+    modal.show();
   }
 }
 
