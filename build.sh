dd78733ae7633ce98327eab47693cd9f36aa4c8c
rtcfirefly <rtcfirefly@users.noreply.github.com>
2023-09-27 10:38:50 -0700

    fix chrome extension packaging

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ make download_resources
 
 OUTPUT_ZIP="ollama-ui.zip"
 
-zip "$OUTPUT_ZIP" ./*.js ./*.css ./*.json ./*.html ./*.png resources/
+zip "$OUTPUT_ZIP" ./*.js ./*.css ./*.json ./*.html ./*.png resources/ resources/*
 
 echo "Files have been zipped as $OUTPUT_ZIP"
